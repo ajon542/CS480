0cf648332537a3180773ba245830c96b2c9fb3ab
Andrew Jones <ajon542@gmail.com>
2016-08-31 13:50:05 -0700

    Use bitmap instead of FillRectangle

--- Lesson1/Mandelbrot/Mandelbrot/Mandelbrot.cs
+++ Lesson1/Mandelbrot/Mandelbrot/Mandelbrot.cs
@@ -80,36 +80,41 @@ namespace Mandelbrot
             // Ensure the graphics context is disposed.
             using (Graphics graphics = CreateGraphics())
             {
-                // Define the limits of the x-y coordinate system.
-                double xMax = 1.1;
-                double xMin = -2;
-                double yMax = 1;
-                double yMin = -1;
-
-                // Scale to width and height of the form.
-                double real = (xMax - xMin) / (Width - 1);
-                double imag = (yMax - yMin) / (Height - 1);
-
-                // Determine the color of each pixel.
-                double c = xMin;
-                for (int x = 0; x < Width; ++x)
+                // Ensure the bitmap is disposed after using.
+                using (Bitmap bitmap = new Bitmap(Width, Height))
                 {
-                    double d = yMin;
-                    for (int y = 0; y < Height; ++y)
+                    // Define the limits of the x-y coordinate system.
+                    double xMax = 1.1;
+                    double xMin = -2;
+                    double yMax = 1;
+                    double yMin = -1;
+
+                    // Scale to width and height of the form.
+                    double real = (xMax - xMin) / (Width - 1);
+                    double imag = (yMax - yMin) / (Height - 1);
+
+                    // Determine the color of each pixel.
+                    double c = xMin;
+                    for (int x = 0; x < Width; ++x)
                     {
-                        // Perform the iterations. For Mandelbrot, iterate from z = 0.
-                        Complex Z = new Complex(0, 0);
-                        Complex C = new Complex(c, d);
-                        int iterations = QuadraticIteration(Z, C);
+                        double d = yMin;
+                        for (int y = 0; y < Height; ++y)
+                        {
+                            // Perform the iterations. For Mandelbrot, iterate from z = 0.
+                            Complex Z = new Complex(0, 0);
+                            Complex C = new Complex(c, d);
+                            int iterations = QuadraticIteration(Z, C);
 
-                        // Draw color.
-                        SolidBrush brush = new SolidBrush(colors[iterations % colors.Count]);
-                        Rectangle rect = new Rectangle(x, y, 1, 1);
-                        graphics.FillRectangle(brush, rect);
+                            // Set pixel color on bitmap.
+                            bitmap.SetPixel(x, y, colors[iterations % colors.Count]);
 
-                        d += imag;
+                            d += imag;
+                        }
+                        c += real;
                     }
-                    c += real;
+
+                    // Draw bitmap image.
+                    e.Graphics.DrawImage(bitmap, 0, 0, bitmap.Width, bitmap.Height);
                 }
             }
         }
