ed4976d19b100c0378535d5288fd4a19025f76b2
Andrew Jones <ajon542@gmail.com>
2016-09-17 16:13:16 -0700

    Added Shape base class.

---
+++ Lesson3/Windmill/Windmill/Shape.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Windmill
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double x, double y, double width, double height)
+        {
+            Points.Add(new Vector2(x, y));
+            Points.Add(new Vector2(x, y + height));
+            Points.Add(new Vector2(x + width, y + height));
+            Points.Add(new Vector2(x + width, y));
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public Circle(Vector2 center, double radius)
+        {
+            Vector2 point = new Vector2(center.x + radius, center.y);
+            Points.Add(point);
+            for (int i = 1; i < 360; ++i)
+            {
+                point = Shape.Rotate(point, center, 1);
+                Points.Add(point);
+            }
+        }
+    }
+
+    public class Shape
+    {
+        public List<Vector2> Points { get; set; }
+
+        public Shape()
+        {
+            Points = new List<Vector2>();
+        }
+
+        public virtual void Rotate(Vector2 anchor, float angle)
+        {
+            for (int i = 0; i < Points.Count; ++i)
+            {
+                Points[i] = Shape.Rotate(Points[i], anchor, angle);
+            }
+        }
+
+        public virtual void Scale(Vector2 anchor, Vector2 scale)
+        {
+            for (int i = 0; i < Points.Count; ++i)
+            {
+                Points[i] = Shape.Scale(Points[i], anchor, scale);
+            }
+        }
+
+        public virtual void Translate(Vector2 translate)
+        {
+            foreach (Vector2 point in Points)
+            {
+                point.x += translate.x;
+                point.y += translate.y;
+            }
+        }
+
+        public virtual Point[] GetPoints()
+        {
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < Points.Count; ++i)
+            {
+                points.Add(new Point((int)Points[i].x, (int)Points[i].y));
+            }
+            return points.ToArray();
+        }
+
+        /// <summary>
+        /// Rotate a point around an anchor.
+        /// </summary>
+        /// <param name="point">The point to rotate.</param>
+        /// <param name="anchor">The anchor to rotate about.</param>
+        /// <param name="angle">The angle to rotate in degrees.</param>
+        /// <returns>The rotated point.</returns>
+        public static Vector2 Rotate(Vector2 point, Vector2 anchor, float angle)
+        {
+            double x = point.x;
+            double y = point.y;
+
+            double x1 = anchor.x;
+            double y1 = anchor.y;
+
+            double rad = angle * Math.PI / 180;
+            double x2 = Math.Cos(rad) * (x - x1) + Math.Sin(rad) * (y1 - y) + x1;
+            double y2 = Math.Sin(rad) * (x - x1) + Math.Cos(rad) * (y - y1) + y1;
+
+            Vector2 result = new Vector2(x2, y2);
+            return result;
+        }
+
+        /// <summary>
+        /// Scale a point about an anchor.
+        /// </summary>
+        /// <param name="point">The point to scale.</param>
+        /// <param name="anchor">The anchor to scale about.</param>
+        /// <param name="scale">The scale factor.</param>
+        /// <returns>A vector representing the position of the scale point.</returns>
+        public static Vector2 Scale(Vector2 point, Vector2 anchor, Vector2 scale)
+        {
+            double x = point.x;
+            double y = point.y;
+
+            double x1 = anchor.x;
+            double y1 = anchor.y;
+
+            double x2 = scale.x * (x - x1) + x1;
+            double y2 = scale.y * (y - y1) + y1;
+
+            Vector2 result = new Vector2(x2, y2);
+            return result;
+        }
+    }
+}
--- Lesson3/Windmill/Windmill/WindmillApp.cs
+++ Lesson3/Windmill/Windmill/WindmillApp.cs
@@ -6,7 +6,7 @@ namespace Windmill
 {
     public class WindmillApp : GameApp
     {
-        private List<Vector2> rectangle;
+        private Shape rectangle;
         private double elapsedTime;
 
         /// <summary>
@@ -14,13 +14,7 @@ namespace Windmill
         /// </summary>
         public override void Initialize()
         {
-            rectangle = new List<Vector2>
-            {
-                new Vector2(200, 200),
-                new Vector2(200, 600),
-                new Vector2(600, 600),
-                new Vector2(600, 200)
-            };
+            rectangle = new Rectangle(100, 100, 200, 400);
         }
 
         /// <summary>
@@ -32,10 +26,7 @@ namespace Windmill
 
             if (elapsedTime > 0.01)
             {
-                for (int i = 0; i < rectangle.Count; ++i)
-                {
-                    rectangle[i] = Rotate(rectangle[i], new Vector2(400, 400), 1);
-                }
+                rectangle.Rotate(new Vector2(400, 400), 1);
                 elapsedTime = 0;
             }
         }
@@ -50,58 +41,7 @@ namespace Windmill
         /// <param name="buffer">THe graphics buffer.</param>
         public override void Render(BufferedGraphics buffer)
         {
-            List<Point> points = new List<Point>();
-            for (int i = 0; i < rectangle.Count; ++i)
-            {
-                points.Add(new Point((int)rectangle[i].x, (int)rectangle[i].y));
-            }
-
-            buffer.Graphics.DrawPolygon(Pens.Blue, points.ToArray());
-        }
-
-        /// <summary>
-        /// Rotate a point around an anchor.
-        /// </summary>
-        /// <param name="point">The point to rotate.</param>
-        /// <param name="anchor">The anchor to rotate about.</param>
-        /// <param name="angle">The angle to rotate in degrees.</param>
-        /// <returns>The rotated point.</returns>
-        private Vector2 Rotate(Vector2 point, Vector2 anchor, float angle)
-        {
-            double x = point.x;
-            double y = point.y;
-
-            double x1 = anchor.x;
-            double y1 = anchor.y;
-
-            double rad = angle * Math.PI / 180;
-            double x2 = Math.Cos(rad) * (x - x1) + Math.Sin(rad) * (y1 - y) + x1;
-            double y2 = Math.Sin(rad) * (x - x1) + Math.Cos(rad) * (y - y1) + y1;
-
-            Vector2 result = new Vector2(x2, y2);
-            return result;
-        }
-
-        /// <summary>
-        /// Scale a point about an anchor.
-        /// </summary>
-        /// <param name="point">The point to scale.</param>
-        /// <param name="anchor">The anchor to scale about.</param>
-        /// <param name="scale">The scale factor.</param>
-        /// <returns>A vector representing the position of the scale point.</returns>
-        private Vector2 Scale(Vector2 point, Vector2 anchor, Vector2 scale)
-        {
-            double x = point.x;
-            double y = point.y;
-
-            double x1 = anchor.x;
-            double y1 = anchor.y;
-
-            double x2 = scale.x * (x - x1) + x1;
-            double y2 = scale.y * (y - y1) + y1;
-
-            Vector2 result = new Vector2(x2, y2);
-            return result;
+            buffer.Graphics.DrawPolygon(Pens.Blue, rectangle.GetPoints());
         }
     }
 }
