2a78fa653dd63e4165b8498ca8564273a9192fdb
Andrew Jones <ajon542@gmail.com>
2016-09-16 19:13:50 -0700

    Added simple render callback on application idle

--- Lesson3/Windmill/Windmill/Form1.Designer.cs
+++ Lesson3/Windmill/Windmill/Form1.Designer.cs
@@ -30,11 +30,6 @@
         {
             this.DrawRegion = new System.Windows.Forms.PictureBox();
             this.DrawTimeLabel = new System.Windows.Forms.Label();
-            this.TimeLabel = new System.Windows.Forms.Label();
-            this.yCoordLabel = new System.Windows.Forms.Label();
-            this.xCoordLabel = new System.Windows.Forms.Label();
-            this.YLabel = new System.Windows.Forms.Label();
-            this.XLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DrawRegion)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,66 +46,11 @@
             this.DrawTimeLabel.AutoSize = true;
             this.DrawTimeLabel.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.DrawTimeLabel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.DrawTimeLabel.Location = new System.Drawing.Point(57, 710);
+            this.DrawTimeLabel.Location = new System.Drawing.Point(169, 739);
             this.DrawTimeLabel.Name = "DrawTimeLabel";
             this.DrawTimeLabel.Size = new System.Drawing.Size(55, 13);
             this.DrawTimeLabel.TabIndex = 25;
             this.DrawTimeLabel.Text = "DrawTime";
-            // 
-            // TimeLabel
-            // 
-            this.TimeLabel.AutoSize = true;
-            this.TimeLabel.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TimeLabel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.TimeLabel.Location = new System.Drawing.Point(16, 710);
-            this.TimeLabel.Name = "TimeLabel";
-            this.TimeLabel.Size = new System.Drawing.Size(37, 13);
-            this.TimeLabel.TabIndex = 24;
-            this.TimeLabel.Text = "Time:";
-            // 
-            // yCoordLabel
-            // 
-            this.yCoordLabel.AutoSize = true;
-            this.yCoordLabel.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.yCoordLabel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.yCoordLabel.Location = new System.Drawing.Point(16, 736);
-            this.yCoordLabel.Name = "yCoordLabel";
-            this.yCoordLabel.Size = new System.Drawing.Size(19, 13);
-            this.yCoordLabel.TabIndex = 21;
-            this.yCoordLabel.Text = "y:";
-            // 
-            // xCoordLabel
-            // 
-            this.xCoordLabel.AutoSize = true;
-            this.xCoordLabel.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.xCoordLabel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.xCoordLabel.Location = new System.Drawing.Point(16, 723);
-            this.xCoordLabel.Name = "xCoordLabel";
-            this.xCoordLabel.Size = new System.Drawing.Size(19, 13);
-            this.xCoordLabel.TabIndex = 20;
-            this.xCoordLabel.Text = "x:";
-            // 
-            // YLabel
-            // 
-            this.YLabel.AutoSize = true;
-            this.YLabel.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.YLabel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.YLabel.Location = new System.Drawing.Point(39, 736);
-            this.YLabel.Name = "YLabel";
-            this.YLabel.Size = new System.Drawing.Size(43, 13);
-            this.YLabel.TabIndex = 19;
-            this.YLabel.Text = "YCoord";
-            // 
-            // XLabel
-            // 
-            this.XLabel.AutoSize = true;
-            this.XLabel.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.XLabel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.XLabel.Location = new System.Drawing.Point(39, 723);
-            this.XLabel.Name = "XLabel";
-            this.XLabel.Size = new System.Drawing.Size(43, 13);
-            this.XLabel.TabIndex = 18;
-            this.XLabel.Text = "XCoord";
             // 
             // Form1
             // 
@@ -118,11 +58,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 761);
             this.Controls.Add(this.DrawTimeLabel);
-            this.Controls.Add(this.TimeLabel);
-            this.Controls.Add(this.yCoordLabel);
-            this.Controls.Add(this.xCoordLabel);
-            this.Controls.Add(this.YLabel);
-            this.Controls.Add(this.XLabel);
             this.Controls.Add(this.DrawRegion);
             this.Name = "Form1";
             this.Text = "2D Transformations";
@@ -136,11 +71,6 @@
 
         private System.Windows.Forms.PictureBox DrawRegion;
         private System.Windows.Forms.Label DrawTimeLabel;
-        private System.Windows.Forms.Label TimeLabel;
-        private System.Windows.Forms.Label yCoordLabel;
-        private System.Windows.Forms.Label xCoordLabel;
-        private System.Windows.Forms.Label YLabel;
-        private System.Windows.Forms.Label XLabel;
     }
 }
 
--- Lesson3/Windmill/Windmill/Form1.cs
+++ Lesson3/Windmill/Windmill/Form1.cs
@@ -5,70 +5,99 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace Windmill
 {
     public partial class Form1 : Form
     {
-        private TimeSpan paintTime;
+        private int i = 0;
+        private float elapsedTime = 0;
+        private int frames = 0;
+        private float fps = 0;
+        private Stopwatch stopWatch;
+
+        private BufferedGraphicsContext currentContext;
+        private BufferedGraphics buffer;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct NativeMessage
+        {
+            public IntPtr Handle;
+            public uint Message;
+            public IntPtr WParameter;
+            public IntPtr LParameter;
+            public uint Time;
+            public Point Location;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int PeekMessage(out NativeMessage message, IntPtr window, uint filterMin, uint filterMax, uint remove);
 
         public Form1()
         {
             InitializeComponent();
 
-            Paint += new PaintEventHandler(DrawRegion_Paint);
-            DrawRegion.MouseMove += new MouseEventHandler(DrawRegion_MouseMove);
-        }
+            Application.ApplicationExit += OnApplicationExit;
+            Application.Idle += OnApplicationIdle;
 
-        /// <summary>
-        /// Draw the scene.
-        /// </summary>
-        private void DrawRegion_Paint(object sender, PaintEventArgs e)
-        {
-            // Keep track of how long it takes to draw the scene.
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            // This example assumes the existence of a form called Form1.
-            BufferedGraphicsContext currentContext;
-            BufferedGraphics myBuffer;
-            // Gets a reference to the current BufferedGraphicsContext
+            // Get a reference to the current BufferedGraphicsContext.
             currentContext = BufferedGraphicsManager.Current;
-            // Creates a BufferedGraphics instance associated with Form1, and with 
-            // dimensions the same size as the drawing surface of Form1.
-            myBuffer = currentContext.Allocate(DrawRegion.CreateGraphics(),
-               DrawRegion.DisplayRectangle);
 
-            // Draws an ellipse to the graphics buffer.
-            myBuffer.Graphics.DrawEllipse(Pens.Blue, new Rectangle(100, 100, 400, 400));
+            // Create a BufferedGraphics instance associated with the DrawRegion.
+            buffer = currentContext.Allocate(DrawRegion.CreateGraphics(), DrawRegion.DisplayRectangle);
 
-            // This example assumes the existence of a BufferedGraphics instance
-            // called myBuffer.
-            // Renders the contents of the buffer to the drawing surface associated 
-            // with the buffer.
-            myBuffer.Render();
-            // Renders the contents of the buffer to the specified drawing surface.
-            myBuffer.Render(DrawRegion.CreateGraphics());
-
-            myBuffer.Dispose();
-
-            // Stop timing.
-            stopwatch.Stop();
-            paintTime = stopwatch.Elapsed;
+            stopWatch = new Stopwatch();
         }
 
-        /// <summary>
-        /// Handle the mouse move event for live updates on mouse position.
-        /// </summary>
-        private void DrawRegion_MouseMove(Object sender, MouseEventArgs e)
+        private bool IsApplicationIdle()
         {
-            // Display the data.
-            XLabel.Text = e.X.ToString();
-            YLabel.Text = e.Y.ToString();
-            DrawTimeLabel.Text = paintTime.ToString(@"ss") + "s, " + paintTime.ToString(@"fff") + "ms";
+            NativeMessage result;
+            return PeekMessage(out result, IntPtr.Zero, (uint)0, (uint)0, (uint)0) == 0;
+        }
+
+        private void OnApplicationIdle(object sender, EventArgs e)
+        {
+            while (IsApplicationIdle())
+            {
+                stopWatch.Reset();
+                stopWatch.Start();
+
+                // Clear the buffer.
+                buffer.Graphics.Clear(Color.Black);
+
+                // Draw an ellipse to the graphics buffer.
+                for (int j = 0; j < 100; ++j)
+                    buffer.Graphics.DrawEllipse(Pens.Blue, new Rectangle(i, 100, 400, 400));
+                i++;
+                if (i > 400)
+                    i = 0;
+                // Render the contents of the buffer to the drawing surface.
+                buffer.Render();
+
+                stopWatch.Stop();
+
+                // Calculate and display frames per second.
+                float renderTime = stopWatch.ElapsedMilliseconds;
+                elapsedTime += renderTime;
+                if (elapsedTime > 500)
+                {
+                    fps = 1000f / (elapsedTime / frames);
+                    DrawTimeLabel.Text = string.Format("fps: {0:000.00}", fps);
+                    frames = 0;
+                    elapsedTime = 0;
+                }
+                frames++;
+            }
+        }
+
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            buffer.Dispose();
         }
     }
 }
