abbfdf3c39695230c207625a78609d4ba0f80fb6
Andrew Jones <ajon542@gmail.com>
2016-09-19 21:33:53 -0700

    Added the windmill.

---
+++ Lesson3/Windmill/Windmill/Shapes/Triangle.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Windmill.Shapes
+{
+    public class Triangle : Shape
+    {
+        public Triangle(Vector2 a, Vector2 b, Vector2 c)
+        {
+            Points.Add(a);
+            Points.Add(b);
+            Points.Add(c);
+        }
+    }
+}
--- Lesson3/Windmill/Windmill/WindmillApp.cs
+++ Lesson3/Windmill/Windmill/WindmillApp.cs
@@ -8,27 +8,36 @@ namespace Windmill
 {
     public class WindmillApp : GameApp
     {
-        private Random random = new Random();
         private List<Shape> shapes = new List<Shape>();
         private double elapsedTime;
+        private Shape stand;
 
         /// <summary>
         /// Initialize the scene.
         /// </summary>
         public override void Initialize()
         {
-            // Create the propeller from two scaled circles.
-            Vector2 position = new Vector2(400, 200);
-            Shape leftBlade = new Circle(position, 50);
-            Shape rightBlade = new Circle(position, 50);
-            leftBlade.Scale(position, new Vector2(2, 0.3));
-            rightBlade.Scale(position, new Vector2(2, 0.3));
+            // Create the propeller blades from triangles.
+            Shape lBlade = new Triangle(new Vector2(0, 0), new Vector2(150, 0), new Vector2(10, 10));
+            Shape rBlade = new Triangle(new Vector2(0, 0), new Vector2(150, 0), new Vector2(10, 10));
+            Shape tBlade = new Triangle(new Vector2(0, 0), new Vector2(150, 0), new Vector2(10, 10));
 
-            leftBlade.Translate(new Vector2(-100, 0));
-            rightBlade.Translate(new Vector2(100, 0));
+            stand = new Triangle(new Vector2(0, 0), new Vector2(-20, 300), new Vector2(20, 300));
 
-            shapes.Add(leftBlade);
-            shapes.Add(rightBlade);
+            // Rotate the blades so they are 120 degrees apart.
+            rBlade.Rotate(new Vector2(0, 0), 120);
+            tBlade.Rotate(new Vector2(0, 0), 240);
+
+            // Translate the blades to their location.
+            lBlade.Translate(new Vector2(400, 200));
+            rBlade.Translate(new Vector2(400, 200));
+            tBlade.Translate(new Vector2(400, 200));
+            stand.Translate(new Vector2(400, 200));
+
+            // Add the blades to the list of shapes for rendering.
+            shapes.Add(lBlade);
+            shapes.Add(rBlade);
+            shapes.Add(tBlade);
         }
 
         /// <summary>
@@ -42,7 +51,7 @@ namespace Windmill
             {
                 foreach (Shape shape in shapes)
                 {
-                    shape.Rotate(new Vector2(400, 200), 1);
+                    shape.Rotate(new Vector2(400, 200), -1);
                 }
                 elapsedTime = 0;
             }
@@ -57,6 +66,7 @@ namespace Windmill
             {
                 buffer.Graphics.DrawPolygon(Pens.Blue, shape.GetPoints());
             }
+            buffer.Graphics.DrawPolygon(Pens.Blue, stand.GetPoints());
         }
     }
 }
