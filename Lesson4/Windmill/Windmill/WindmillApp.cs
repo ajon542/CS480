c310befe37c08197d8715e11cb658d2cfc518f74
Andrew Jones <ajon542@gmail.com>
2016-09-20 15:00:28 -0700

    Added background image.

--- Lesson4/Windmill/Windmill/WindmillApp.cs
+++ Lesson4/Windmill/Windmill/WindmillApp.cs
@@ -14,9 +14,6 @@ namespace Windmill
         private List<Shape> blades = new List<Shape>();
         private double elapsedTime;
         private Shape stand;
-        private Shape grass;
-        private Shape sky;
-        private List<Shape> cloud;
 
         private SolidBrush silverBrush = new SolidBrush(Color.Silver);
         private SolidBrush brownBrush = new SolidBrush(Color.Brown);
@@ -24,11 +21,16 @@ namespace Windmill
         private SolidBrush blueBrush = new SolidBrush(Color.LightBlue);
         private SolidBrush whiteBrush = new SolidBrush(Color.WhiteSmoke);
 
+        private Bitmap background;
+
         /// <summary>
         /// Initialize the scene.
         /// </summary>
         public override void Initialize()
         {
+            // Load the background image.
+            background = new Bitmap("Assets/Background.jpg");
+
             // Create the propeller blades from triangles.
             Shape lBlade = new Shapes.Triangle(new Vector2(0, 0), new Vector2(150, 0), new Vector2(10, 20));
             Shape rBlade = new Shapes.Triangle(new Vector2(0, 0), new Vector2(150, 0), new Vector2(10, 20));
@@ -36,25 +38,6 @@ namespace Windmill
 
             // Create the base of the windmill.
             stand = new Shapes.Triangle(new Vector2(0, 0), new Vector2(-20, 300), new Vector2(20, 300));
-
-            // Create the grass.
-            grass = new Shapes.Rectangle(0, 500, 800, 300);
-
-            // Create the sky.
-            sky = new Shapes.Rectangle(0, 0, 800, 500);
-
-            // Create the clouds. (Probably a little inefficient to use circles).
-            cloud = new List<Shape>();
-            cloud.Add(new Circle(new Vector2(0, 0), 50));
-            cloud.Add(new Circle(new Vector2(60, 10), 35));
-            cloud.Add(new Circle(new Vector2(100, 10), 25));
-            cloud.Add(new Circle(new Vector2(0, 0), 50));
-            cloud.Add(new Circle(new Vector2(-60, 10), 40));
-            cloud.Add(new Circle(new Vector2(-100, 15), 20));
-            foreach (Shape shape in cloud)
-            {
-                shape.Translate(new Vector2(100, 100));
-            }
 
             // Rotate the blades so they are 120 degrees apart.
             rBlade.Rotate(new Vector2(0, 0), 120);
@@ -82,11 +65,6 @@ namespace Windmill
             // Rotate the blades.
             if (elapsedTime > 10)
             {
-                foreach (Shape shape in cloud)
-                {
-                    shape.Translate(new Vector2(0.2, 0));
-                }
-
                 foreach (Shape shape in blades)
                 {
                     shape.Rotate(new Vector2(400, 200), -1);
@@ -100,20 +78,20 @@ namespace Windmill
         /// </summary>
         public override void Render(Graphics graphics)
         {
-            // Draw the grass, sky, windmill stand and blades.
-            graphics.FillPolygon(blueBrush, sky.GetPoints(), FillMode.Winding);
-            graphics.FillPolygon(greenBrush, grass.GetPoints(), FillMode.Winding);
+            graphics.DrawImage(background, 0, 0);
             graphics.FillPolygon(brownBrush, stand.GetPoints(), FillMode.Winding);
-
-            foreach (Shape shape in cloud)
-            {
-                graphics.FillPolygon(whiteBrush, shape.GetPoints(), FillMode.Winding);
-            }
-
             foreach(Shape blade in blades)
             {
                 graphics.FillPolygon(silverBrush, blade.GetPoints(), FillMode.Winding);
             }
+        }
+
+        /// <summary>
+        /// Dispose asets.
+        /// </summary>
+        public override void Shutdown()
+        {
+            background.Dispose();
         }
     }
 }
