b9b000420f9084603eb9071b3b1659b673590cb8
Andrew Jones <ajon542@gmail.com>
2016-09-30 13:56:14 -0700

    Check in the guts of the bresenham application

---
+++ Lesson5/Bresenham/Bresenham/Core/GameApp.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace GameEngine.Core
+{
+    /// <summary>
+    /// Abstract game application class.
+    /// </summary>
+    public class GameApp
+    {
+        /// <summary>
+        /// Initialize the game application.
+        /// </summary>
+        public virtual void Initialize() { }
+
+        /// <summary>
+        /// Update the game application.
+        /// </summary>
+        public virtual void Update(double deltaTime) { }
+
+        /// <summary>
+        /// Render the game application.
+        /// </summary>
+        /// <param name="graphics">THe graphics buffer.</param>
+        public virtual void Render(Graphics graphics) { }
+
+        /// <summary>
+        /// Shutdown the game application.
+        /// </summary>
+        public virtual void Shutdown() { }
+    }
+}
---
+++ Lesson5/Bresenham/Bresenham/Core/Vector2.cs
@@ -0,0 +1,15 @@
+﻿
+namespace GameEngine.Core
+{
+    public class Vector2
+    {
+        public double x;
+        public double y;
+
+        public Vector2(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
