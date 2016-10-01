8a97b3f1035659e21bb7df6cb2c5c0e4f4ef8f2a
Andrew Jones <ajon542@gmail.com>
2016-10-01 14:00:14 -0700

    Removed unused usings

--- Lesson5/Bresenham/Bresenham/BresenhamApp.cs
+++ Lesson5/Bresenham/Bresenham/BresenhamApp.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Windows.Forms;
 
 using GameEngine.Core;
 using GameEngine.Core.Shapes;
