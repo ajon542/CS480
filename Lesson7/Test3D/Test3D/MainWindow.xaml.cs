b3249410a64136a4143dee7eafe2a54a994b0988
Andrew Jones <ajon542@gmail.com>
2016-10-05 15:54:20 -0700

    Added ray hit handler
    The example just changes material color

--- Lesson7/Test3D/Test3D/MainWindow.xaml.cs
+++ Lesson7/Test3D/Test3D/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
 using System.Diagnostics;
@@ -12,6 +13,8 @@ namespace Test3D
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -41,6 +44,40 @@ namespace Test3D
             ModelVisual3D leftWall = ShapeGenerator.CreatePlane(50, ShapeGenerator.GetSimpleMaterial(Color.FromArgb(255, 200, 191, 231)));
             leftWall.Transform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 0, 1), -90));
             mainViewport.Children.Add(leftWall);
+
+            MouseDown += HitTest;
+        }
+
+        private void HitTest(object sender, MouseButtonEventArgs args)
+        {
+            Point mouseposition = args.GetPosition(mainViewport);
+            Point3D testpoint3D = new Point3D(mouseposition.X, mouseposition.Y, 0);
+            Vector3D testdirection = new Vector3D(mouseposition.X, mouseposition.Y, 10);
+            PointHitTestParameters pointparams = new PointHitTestParameters(mouseposition);
+            RayHitTestParameters rayparams = new RayHitTestParameters(testpoint3D, testdirection);
+
+            // Test for a result in the Viewport3D
+            VisualTreeHelper.HitTest(mainViewport, null, HitTestResult, pointparams);
+        }
+
+        private HitTestResultBehavior HitTestResult(HitTestResult rawresult)
+        {
+            RayHitTestResult rayResult = rawresult as RayHitTestResult;
+
+            if (rayResult != null)
+            {
+                RayMeshGeometry3DHitTestResult rayMeshResult = rayResult as RayMeshGeometry3DHitTestResult;
+
+                if (rayMeshResult != null)
+                {
+                    GeometryModel3D hitgeo = rayMeshResult.ModelHit as GeometryModel3D;
+
+                    // Change to blue material 
+                    hitgeo.Material = ShapeGenerator.GetSimpleMaterial(Colors.Red);
+                }
+            }
+
+            return HitTestResultBehavior.Stop;
         }
     }
 }
