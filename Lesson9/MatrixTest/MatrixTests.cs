9ad6f0cd63620bb44b0e55fba087434d698e78cf
Andrew Jones <ajon542@gmail.com>
2016-10-22 12:55:02 -0700

    Added more matrix tests

--- Lesson9/MatrixTest/MatrixTests.cs
+++ Lesson9/MatrixTest/MatrixTests.cs
@@ -8,6 +8,8 @@ namespace MatrixTest
     [TestClass]
     public class MatrixTests
     {
+        #region Row Multiply
+
         [TestMethod]
         public void TestRowMulitply1()
         {
@@ -25,5 +27,66 @@ namespace MatrixTest
             Assert.AreEqual(3, m.Data[1, 0]);
             Assert.AreEqual(4, m.Data[1, 1]);
         }
+
+        [TestMethod]
+        public void TestRowMulitply2()
+        {
+            TridiagonalMatrix m = new TridiagonalMatrix(2);
+
+            m.Data[0, 0] = 1;
+            m.Data[0, 1] = 2;
+            m.Data[1, 0] = 3;
+            m.Data[1, 1] = 4;
+
+            m.RowMultiply(1, -2);
+
+            Assert.AreEqual(1, m.Data[0, 0]);
+            Assert.AreEqual(2, m.Data[0, 1]);
+            Assert.AreEqual(-6, m.Data[1, 0]);
+            Assert.AreEqual(-8, m.Data[1, 1]);
+        }
+
+        #endregion
+
+        #region Row Subtract
+
+
+        [TestMethod]
+        public void TestRowSubtract1()
+        {
+            TridiagonalMatrix m = new TridiagonalMatrix(2);
+
+            m.Data[0, 0] = 1;
+            m.Data[0, 1] = 2;
+            m.Data[1, 0] = 3;
+            m.Data[1, 1] = 4;
+
+            m.RowSubtract(0, 1);
+
+            Assert.AreEqual(-2, m.Data[0, 0]);
+            Assert.AreEqual(-2, m.Data[0, 1]);
+            Assert.AreEqual(3, m.Data[1, 0]);
+            Assert.AreEqual(4, m.Data[1, 1]);
+        }
+
+        [TestMethod]
+        public void TestRowSubtract2()
+        {
+            TridiagonalMatrix m = new TridiagonalMatrix(2);
+
+            m.Data[0, 0] = 1;
+            m.Data[0, 1] = 2;
+            m.Data[1, 0] = 3;
+            m.Data[1, 1] = 4;
+
+            m.RowSubtract(1, 0);
+
+            Assert.AreEqual(1, m.Data[0, 0]);
+            Assert.AreEqual(2, m.Data[0, 1]);
+            Assert.AreEqual(2, m.Data[1, 0]);
+            Assert.AreEqual(2, m.Data[1, 1]);
+        }
+
+        #endregion
     }
 }
