087ce1ac77c9e6ba733ccd119cee6d1f73528c8f
Andrew Jones <ajon542@gmail.com>
2016-10-21 17:56:38 -0700

    Added fraction equality and tests

---
+++ Lesson9/FractionTest/FractionTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using comb1;
+
+namespace FractionTest
+{
+    [TestClass]
+    public class FractionTests
+    {
+        #region Test Construction and Value
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Fraction f = new Fraction(1, 2);
+            Assert.AreEqual(0.5, f.Value);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Fraction f = new Fraction(2, 1);
+            Assert.AreEqual(2, f.Value);
+        }
+
+        #endregion
+
+        #region Test Equality
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(new Fraction(2, 1), new Fraction(4, 2));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(new Fraction(1, 3), new Fraction(1, 3));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreNotEqual(new Fraction(2, 3), new Fraction(4, 2));
+        }
+
+        #endregion
+    }
+}
--- Lesson9/comb1/Fraction.cs
+++ Lesson9/comb1/Fraction.cs
@@ -14,7 +14,7 @@ namespace comb1
         public int Numerator { get; set; }
         public int Denominator { get; set; }
 
-        public double Value { get { return Numerator / Denominator; } }
+        public double Value { get { return (double)Numerator / Denominator; } }
 
         public Fraction(int numerator, int denominator)
         {
@@ -25,6 +25,56 @@ namespace comb1
         // TODO: Add and subtract two fractions
         // TODO: Multiply and divide two fractions
         // TODO: Reduce a fraction to the smallest numerator and denominator
-        // TODO: Equality of two fractions
+
+        /// <summary>
+        /// Equals override.
+        /// </summary>
+        /// <param name="other">The object to compare.</param>
+        /// <returns>True if the object is the same; false otherwise.</returns>
+        public override bool Equals(object other)
+        {
+            // If parameter is null return false.
+            if (other == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            Fraction fraction = other as Fraction;
+            if ((object)fraction == null)
+            {
+                return false;
+            }
+
+            // Return true if the fractional value matches.
+            return fraction.Value == Value;
+        }
+
+        /// <summary>
+        /// Equals override.
+        /// </summary>
+        /// <param name="other">The object to compare.</param>
+        /// <returns>True if the object is the same; false otherwise.</returns>
+        public bool Equals(Fraction other)
+        {
+            // If parameter is null return false:
+            if ((object)other == null)
+            {
+                return false;
+            }
+
+            // Return true if the fractional value matches.
+            return other.Value == Value;
+        }
+
+        /// <summary>
+        /// Convert the hash value of the card.
+        /// </summary>
+        /// <returns>The hash value of the card.</returns>
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
     }
 }
