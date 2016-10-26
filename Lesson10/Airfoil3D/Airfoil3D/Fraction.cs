06b2eba95a53d0429c509f36bc3899b706585059
Andrew Jones <ajon542@gmail.com>
2016-10-26 12:52:07 -0700

    Added all the curve classes needed

---
+++ Lesson10/Airfoil3D/Airfoil3D/Fraction.cs
@@ -0,0 +1,201 @@
+﻿using System;
+
+namespace Airfoil3D
+{
+    /// <summary>
+    /// Class to represent rational numbers using a fraction.
+    /// </summary>
+    public class Fraction
+    {
+        public int Numerator { get; private set; }
+        public int Denominator { get; private set; }
+
+        public double Value { get { return (double)Numerator / Denominator; } }
+
+        #region Contructors
+
+        public Fraction(int numerator)
+        {
+            Numerator = numerator;
+            Denominator = 1;
+        }
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("denominator cannot be zero");
+            }
+
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public static implicit operator Fraction(int value)
+        {
+            return new Fraction(value);
+        }
+
+        #endregion
+
+        #region Operator Overloads
+
+        /// <summary>
+        /// Overload the addition operator.
+        /// </summary>
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            int numerator = f1.Numerator * f2.Denominator + f2.Numerator * f1.Denominator;
+            int denominator = f1.Denominator * f2.Denominator;
+
+            Fraction result = new Fraction(numerator, denominator);
+            result.Reduce();
+            return result;
+        }
+
+        /// <summary>
+        /// Overload the subtraction operator.
+        /// </summary>
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            int numerator = f1.Numerator * f2.Denominator - f2.Numerator * f1.Denominator;
+            int denominator = f1.Denominator * f2.Denominator;
+
+            Fraction result = new Fraction(numerator, denominator);
+            result.Reduce();
+            return result;
+        }
+
+        /// <summary>
+        /// Overload the multiplication operator.
+        /// </summary>
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            Fraction result = new Fraction(f1.Numerator * f2.Numerator, f1.Denominator * f2.Denominator);
+            result.Reduce();
+            return result;
+        }
+
+        /// <summary>
+        /// Overload the division operator.
+        /// </summary>
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            Fraction result = new Fraction(f1.Numerator * f2.Denominator, f1.Denominator * f2.Numerator);
+            result.Reduce();
+            return result;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Returns a fraction that if multiplied by the first argument will result
+        /// in a fraction that equals the second argument.
+        /// </summary>
+        public static Fraction Convert(Fraction f1, Fraction f2)
+        {
+            Fraction result = new Fraction(f1.Denominator * f2.Numerator, f1.Numerator * f2.Denominator);
+            result.Reduce();
+            return result;
+        }
+
+        /// <summary>
+        /// Invert the fraction.
+        /// </summary>
+        public void Invert()
+        {
+            if (Numerator == 0)
+            {
+                throw new ArgumentException("numerator cannot be zero when inverting");
+            }
+
+            int temp = Numerator;
+            Numerator = Denominator;
+            Denominator = temp;
+        }
+
+        /// <summary>
+        /// Reduce the fraction to the smallest possible numerator and denominator.
+        /// </summary>
+        public void Reduce()
+        {
+            int gcd = Gcd(Numerator, Denominator);
+
+            Numerator /= gcd;
+            Denominator /= gcd;
+        }
+
+        /// <summary>
+        /// Find the greatest common divisor.
+        /// </summary>
+        private int Gcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+        #region Equality
+
+        /// <summary>
+        /// Equals override.
+        /// </summary>
+        /// <param name="other">The object to compare.</param>
+        /// <returns>True if the object is the same; false otherwise.</returns>
+        public override bool Equals(object other)
+        {
+            // If parameter is null return false.
+            if (other == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            Fraction fraction = other as Fraction;
+            if ((object)fraction == null)
+            {
+                return false;
+            }
+
+            // Return true if the fractional value matches.
+            return fraction.Value == Value;
+        }
+
+        /// <summary>
+        /// Equals override.
+        /// </summary>
+        /// <param name="other">The object to compare.</param>
+        /// <returns>True if the object is the same; false otherwise.</returns>
+        public bool Equals(Fraction other)
+        {
+            // If parameter is null return false.
+            if ((object)other == null)
+            {
+                return false;
+            }
+
+            // Return true if the fractional value matches.
+            return other.Value == Value;
+        }
+
+        /// <summary>
+        /// Convert the hash value of the card.
+        /// </summary>
+        /// <returns>The hash value of the card.</returns>
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", Numerator, Denominator);
+        }
+    }
+}
---
+++ Lesson10/Airfoil3D/Airfoil3D/NaturalSpline.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Media.Media3D;
+
+namespace Airfoil3D
+{
+    public class NaturalSpline
+    {
+        public List<Vector3D> Points { get; set; }
+        private List<Vector3D> controlPoints;
+
+        private TridiagonalMatrix m;
+
+        public NaturalSpline(List<Vector3D> controlPoints)
+        {
+            this.controlPoints = controlPoints;
+
+            // Create a tridiagonal matrix using 5 control points and invert.
+            m = new TridiagonalMatrix(5);
+
+            m.Data[0, 0] = 2; m.Data[0, 1] = 1; m.Data[0, 2] = 0; m.Data[0, 3] = 0; m.Data[0, 4] = 0;
+            m.Data[1, 0] = 1; m.Data[1, 1] = 4; m.Data[1, 2] = 1; m.Data[1, 3] = 0; m.Data[1, 4] = 0;
+            m.Data[2, 0] = 0; m.Data[2, 1] = 1; m.Data[2, 2] = 4; m.Data[2, 3] = 1; m.Data[2, 4] = 0;
+            m.Data[3, 0] = 0; m.Data[3, 1] = 0; m.Data[3, 2] = 1; m.Data[3, 3] = 4; m.Data[3, 4] = 1;
+            m.Data[4, 0] = 0; m.Data[4, 1] = 0; m.Data[4, 2] = 0; m.Data[4, 3] = 1; m.Data[4, 4] = 2;
+
+            // The result of the matrix inversion can be used to calculate the tangents
+            // at each of the control points.
+            m.Invert();
+
+            // Obtain the tangents for each of the control points.
+            Vector3D t0;
+            Vector3D t1;
+            Vector3D t2;
+            Vector3D t3;
+            Vector3D t4;
+            GetTangent(out t0, 0);
+            GetTangent(out t1, 1);
+            GetTangent(out t2, 2);
+            GetTangent(out t3, 3);
+            GetTangent(out t4, 4);
+
+            // Generate a curve between each of the points using the Hermite basis functions.
+            HermiteCurve h0 = new HermiteCurve(controlPoints[0], controlPoints[1], t0, t1);
+            HermiteCurve h1 = new HermiteCurve(controlPoints[1], controlPoints[2], t1, t2);
+            HermiteCurve h2 = new HermiteCurve(controlPoints[2], controlPoints[3], t2, t3);
+            HermiteCurve h3 = new HermiteCurve(controlPoints[3], controlPoints[4], t3, t4);
+
+            // Add the points to the overall curve.
+            Points = new List<Vector3D>();
+            Points.AddRange(h0.Points);
+            Points.AddRange(h1.Points);
+            Points.AddRange(h2.Points);
+            Points.AddRange(h3.Points);
+        }
+
+        private void GetTangent(out Vector3D t, int i)
+        {
+            t = new Vector3D();
+            t += m.Inverse[i, 0].Value * 3 * (controlPoints[1] - controlPoints[0]);
+            t += m.Inverse[i, 1].Value * 3 * (controlPoints[2] - controlPoints[0]);
+            t += m.Inverse[i, 2].Value * 3 * (controlPoints[3] - controlPoints[1]);
+            t += m.Inverse[i, 3].Value * 3 * (controlPoints[4] - controlPoints[2]);
+            t += m.Inverse[i, 4].Value * 3 * (controlPoints[4] - controlPoints[3]);
+        }
+    }
+}
