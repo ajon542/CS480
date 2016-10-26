cc9556e0c3b10c902d0996ee6bad8329125f51f1
Andrew Jones <ajon542@gmail.com>
2016-10-26 12:02:50 -0700

    Added lesson 10

---
+++ Lesson10/Airfoil3D/Airfoil3D/GameModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Media.Media3D;
+
+namespace Airfoil3D
+{
+    /// <summary>
+    /// Represent a 3D model.
+    /// </summary>
+    public class GameModel
+    {
+        /// <summary>
+        /// Obtain the points of the model in world coordinates.
+        /// </summary>
+        /// <returns>The points of the model in world coordinates.</returns>
+        public Matrix3D ComputeWorldCoordinates()
+        {
+            return new Matrix3D();
+        }
+    }
+}
