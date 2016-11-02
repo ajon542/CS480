82ea316a9f0f0e2ce09edcd38e81756c15b79021
Andrew Jones <ajon542@gmail.com>
2016-11-02 11:49:02 -0700

    Setup basic 3d scene and camera movement

--- Lesson11/AirfoilSurface/AirfoilSurface/MainWindow.xaml.cs
+++ Lesson11/AirfoilSurface/AirfoilSurface/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
 
 namespace AirfoilSurface
 {
@@ -19,9 +11,105 @@ namespace AirfoilSurface
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int mouseWheelIndex = 0;
+        private double vAngle = 60;
+        private double hAngle = 60;
+        private double prevX = 0, prevY = 0;
+        private double distance = 30;
+
+
         public MainWindow()
         {
+
             InitializeComponent();
+
+            // Create a basic material.
+            MaterialGroup material = new MaterialGroup();
+            material.Children.Add(ShapeGenerator.GetSimpleMaterial(Colors.LightGreen));
+
+            // Create a sphere.
+            Transform3DGroup transformGroup = new Transform3DGroup();
+            ModelVisual3D sphere = ShapeGenerator.GenerateUnitSphere(30, 30, material);
+            mainViewport.Children.Add(sphere);
+
+            // Create the camera.
+            PerspectiveCamera camera = new PerspectiveCamera();
+            camera.FieldOfView = 60;
+            mainViewport.Camera = camera;
+            UpdateCameraVectors();
+
+            // Setup the event handlers.
+            MouseMove += MouseMoveEventHandler;
+            MouseWheel += MouseWheelHandler;
         }
+
+        /// <summary>
+        /// The camera movement will be looking at the origin while rotating
+        /// around the object in the center of the scene.
+        /// </summary>
+        private void UpdateCameraVectors()
+        {
+            PerspectiveCamera camera = (mainViewport.Camera as PerspectiveCamera);
+
+            double vA = vAngle * Math.PI / 180;
+            double hA = hAngle * Math.PI / 180;
+
+            double x = distance * Math.Sin(vA) * Math.Cos(hA);
+            double y = distance * Math.Cos(vA);
+            double z = distance * Math.Sin(vA) * Math.Sin(hA);
+
+            camera.Position = new Point3D(x, y, z);
+
+            camera.LookDirection = -(Vector3D)camera.Position;
+            camera.LookDirection.Normalize();
+        }
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Handle scene zoom.
+        /// </summary>
+        private void MouseWheelHandler(object sender, MouseWheelEventArgs args)
+        {
+            PerspectiveCamera camera = (mainViewport.Camera as PerspectiveCamera);
+
+            if (mouseWheelIndex != args.Delta)
+            {
+                if (mouseWheelIndex > args.Delta)
+                {
+                    distance++;
+                }
+                if (mouseWheelIndex < args.Delta)
+                {
+                    distance--;
+                }
+            }
+
+            UpdateCameraVectors();
+        }
+
+        /// <summary>
+        /// Handle scene rotation.
+        /// </summary>
+        private void MouseMoveEventHandler(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                double hAngleSpeed = 0.5;
+                double vAngleSpeed = 0.5;
+                hAngle += hAngleSpeed * (e.GetPosition(mainViewport).X - prevX);
+                vAngle -= vAngleSpeed * (e.GetPosition(mainViewport).Y - prevY);
+
+                if (vAngle > 179) vAngle = 179;
+                else if (vAngle < 1) vAngle = 1;
+            }
+
+            prevX = e.GetPosition(mainViewport).X;
+            prevY = e.GetPosition(mainViewport).Y;
+
+            UpdateCameraVectors();
+        }
+
+        #endregion
     }
 }
