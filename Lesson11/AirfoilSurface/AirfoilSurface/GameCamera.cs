5ad63c6eeea607d302ff4f10b9f3d8fec19188e2
Andrew Jones <ajon542@gmail.com>
2016-11-02 12:28:45 -0700

    Moved the camera code into its own class

---
+++ Lesson11/AirfoilSurface/AirfoilSurface/GameCamera.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows.Media.Media3D;
+
+namespace AirfoilSurface
+{
+    public class GameCamera
+    {
+        private int mouseWheelIndex = 0;
+        private double vAngle = 60;
+        private double hAngle = 60;
+        private double prevX = 0, prevY = 0;
+        private double distance = 30;
+
+        public PerspectiveCamera Camera { get; private set; }
+
+        public GameCamera()
+        {
+            // Create the camera.
+            Camera = new PerspectiveCamera();
+            Camera.FieldOfView = 60;
+            UpdateCameraVectors();
+        }
+
+        /// <summary>
+        /// The camera movement will be looking at the origin while rotating
+        /// around the object in the center of the scene.
+        /// </summary>
+        private void UpdateCameraVectors()
+        {
+            double vA = vAngle * Math.PI / 180;
+            double hA = hAngle * Math.PI / 180;
+
+            double x = distance * Math.Sin(vA) * Math.Cos(hA);
+            double y = distance * Math.Cos(vA);
+            double z = distance * Math.Sin(vA) * Math.Sin(hA);
+
+            Camera.Position = new Point3D(x, y, z);
+
+            Camera.LookDirection = -(Vector3D)Camera.Position;
+            Camera.LookDirection.Normalize();
+        }
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Handle scene zoom.
+        /// </summary>
+        public void MouseWheelHandler(object sender, MouseWheelEventArgs args)
+        {
+            if (mouseWheelIndex != args.Delta)
+            {
+                if (mouseWheelIndex > args.Delta)
+                {
+                    distance++;
+                }
+                if (mouseWheelIndex < args.Delta)
+                {
+                    distance--;
+                }
+            }
+
+            UpdateCameraVectors();
+        }
+
+        /// <summary>
+        /// Handle scene rotation.
+        /// </summary>
+        public void MouseMoveEventHandler(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                double hAngleSpeed = 0.5;
+                double vAngleSpeed = 0.5;
+                hAngle += hAngleSpeed * (e.GetPosition(null).X - prevX);
+                vAngle -= vAngleSpeed * (e.GetPosition(null).Y - prevY);
+
+                if (vAngle > 179) vAngle = 179;
+                else if (vAngle < 1) vAngle = 1;
+            }
+
+            prevX = e.GetPosition(null).X;
+            prevY = e.GetPosition(null).Y;
+
+            UpdateCameraVectors();
+        }
+
+        #endregion
+    }
+}
--- Lesson11/AirfoilSurface/AirfoilSurface/MainWindow.xaml.cs
+++ Lesson11/AirfoilSurface/AirfoilSurface/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
 
@@ -11,16 +9,8 @@ namespace AirfoilSurface
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int mouseWheelIndex = 0;
-        private double vAngle = 60;
-        private double hAngle = 60;
-        private double prevX = 0, prevY = 0;
-        private double distance = 30;
-
-
         public MainWindow()
         {
-
             InitializeComponent();
 
             // Create a basic material.
@@ -32,84 +22,13 @@ namespace AirfoilSurface
             ModelVisual3D sphere = ShapeGenerator.GenerateUnitSphere(30, 30, material);
             mainViewport.Children.Add(sphere);
 
-            // Create the camera.
-            PerspectiveCamera camera = new PerspectiveCamera();
-            camera.FieldOfView = 60;
-            mainViewport.Camera = camera;
-            UpdateCameraVectors();
+            // Create and assign the camera.
+            GameCamera camera = new GameCamera();
+            mainViewport.Camera = camera.Camera;
 
             // Setup the event handlers.
-            MouseMove += MouseMoveEventHandler;
-            MouseWheel += MouseWheelHandler;
+            MouseMove += camera.MouseMoveEventHandler;
+            MouseWheel += camera.MouseWheelHandler;
         }
-
-        /// <summary>
-        /// The camera movement will be looking at the origin while rotating
-        /// around the object in the center of the scene.
-        /// </summary>
-        private void UpdateCameraVectors()
-        {
-            PerspectiveCamera camera = (mainViewport.Camera as PerspectiveCamera);
-
-            double vA = vAngle * Math.PI / 180;
-            double hA = hAngle * Math.PI / 180;
-
-            double x = distance * Math.Sin(vA) * Math.Cos(hA);
-            double y = distance * Math.Cos(vA);
-            double z = distance * Math.Sin(vA) * Math.Sin(hA);
-
-            camera.Position = new Point3D(x, y, z);
-
-            camera.LookDirection = -(Vector3D)camera.Position;
-            camera.LookDirection.Normalize();
-        }
-
-        #region Event Handlers
-
-        /// <summary>
-        /// Handle scene zoom.
-        /// </summary>
-        private void MouseWheelHandler(object sender, MouseWheelEventArgs args)
-        {
-            PerspectiveCamera camera = (mainViewport.Camera as PerspectiveCamera);
-
-            if (mouseWheelIndex != args.Delta)
-            {
-                if (mouseWheelIndex > args.Delta)
-                {
-                    distance++;
-                }
-                if (mouseWheelIndex < args.Delta)
-                {
-                    distance--;
-                }
-            }
-
-            UpdateCameraVectors();
-        }
-
-        /// <summary>
-        /// Handle scene rotation.
-        /// </summary>
-        private void MouseMoveEventHandler(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                double hAngleSpeed = 0.5;
-                double vAngleSpeed = 0.5;
-                hAngle += hAngleSpeed * (e.GetPosition(mainViewport).X - prevX);
-                vAngle -= vAngleSpeed * (e.GetPosition(mainViewport).Y - prevY);
-
-                if (vAngle > 179) vAngle = 179;
-                else if (vAngle < 1) vAngle = 1;
-            }
-
-            prevX = e.GetPosition(mainViewport).X;
-            prevY = e.GetPosition(mainViewport).Y;
-
-            UpdateCameraVectors();
-        }
-
-        #endregion
     }
 }
